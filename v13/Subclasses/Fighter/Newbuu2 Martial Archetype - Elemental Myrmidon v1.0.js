eb6dfb592470da7b2865e867b68d5f98d680fa9a
BraabHimself <cory.m.finch@gmail.com>
2021-03-02 16:09:47 -0500

    Update Elemental Myrmidon
    Update with changes from crique

--- v13/Subclasses/Fighter/Newbuu2 Martial Archetype - Elemental Myrmidon v1.0.js
+++ v13/Subclasses/Fighter/Newbuu2 Martial Archetype - Elemental Myrmidon v1.0.js
@@ -11,6 +11,16 @@ SourceList["NB2:EM"] = {
 	date : "2021/02/16"
 };
 
+var elementPreReqFunction = function(v) {
+	var elementsSelected = GetFeatureChoice("classes", "fighter", "subclassfeature3.1", true).length;
+	
+	return (classes.known.fighter.level >= 3 && elementsSelected == 0) || (classes.known.fighter.level >= 18 && elementsSelected < 2);
+};
+
+var elementChangeEvalFunction = function() {
+	ReCalcWeapons(true, true);
+};
+
 AddSubClass("fighter", "elemental myrmidon", {
 	regExpSearch : /^(?=.*(elemental|primordial))(?=.*myrmidon).*$/i,
 	subname : "Elemental Myrmidon",
@@ -22,7 +32,7 @@ AddSubClass("fighter", "elemental myrmidon", {
 			source : ["NB2:EM", 1],
 			minlevel : 3,
 			description : desc([
-				"I learn Primordial; I have adv. on Persuasion checks for interactions with elementals"
+				"I learn Primordial; Primordial is used for attuning to the elements"
 			]),
 			languageProfs : ["Primordial"]
 		},
@@ -31,134 +41,126 @@ AddSubClass("fighter", "elemental myrmidon", {
 			source : ["NB2:EM", 1],
 			minlevel : 3,
 			description : desc([
-				"After a long rest, I can imbue a melee weapon I touch with cold, fire, or lightning",
-				"It deals extra damage of the chosen type equal to my prof. bonus and counts as magical",
-				"Use the \"Choose Feature\" button above to select the imbued type"
+				"After a long rest, I can attune to one of the following damage types:",
+				" \u2022 acid, cold, fire, lightning, or thunder",
+				"I can then imbue a melee weapon with damages type to which I am attuned",
+				"Once per turn, it can deal extra damage of an attuned type equal to my prof. bonus",
+				"I have adv. on Persuasion checks for interactions with elementals while attuned",
+				"Use the \"Choose Feature\" button above to select elemental attunements"
 			]),
-			choices : ["Cold", "Fire", "Lightning"],
-			"cold" : {
-				name : "Myrmidon's Armament: Cold",
-				description : desc([
-					"My melee weapon is cold imbued, it deals extra cold damage; counts as magical",
-					"The extra cold damage is equal to my proficiency bonus",
-					"When I finish a long rest, I can imbue a melee weapon I touch with an element",
-					"Use the \"Choose Feature\" button above to change the imbued type"
-				]),
-				calcChanges : {
-					atkAdd : [
-						function (fields, v) {
-							if (v.isMeleeWeapon && !v.isNaturalWeapon && (/\b(cold imbued)\b/i).test(v.WeaponText)) {
+			calcChanges : {
+				atkAdd : [
+					function (fields, v) {
+						if (v.isMeleeWeapon && !v.isNaturalWeapon && (/\b(imbued)\b/i).test(v.WeaponText)) {
+							var attunements = GetFeatureChoice("classes", "fighter", "subclassfeature3.1", true);
+							
+							if(attunements.length)
+							{
 								var bonusDamage = ProficiencyBonusList[classes.totallevel - 1];
-								
+							
 								if((/\b(supercharged?)\b/i).test(v.WeaponText) && classes.known.fighter.level >= 10)
 								{
 									bonusDamage *= 2;
 								}
 								
-								fields.Description += (fields.Description ? '; ' : '') + "+" + bonusDamage + " cold damage; counts as magical";
+								if(attunements.length == 1)
+								{
+									fields.Description += (fields.Description ? '; ' : '') + "+" + bonusDamage + " " + attunements[0] + " damage";
+								}
+								else
+								{
+									var type1 = bonusDamage + " " + attunements[0] + " damage";
+									var type2 = bonusDamage + " " + attunements[1] + " damage";
+									
+									fields.Description += (fields.Description ? '; ' : '') + "+" + type1 + " or " + type2;
+								}
+								
+								if(classes.known.fighter.level >= 7)
+								{
+									fields.Description += (fields.Description ? '; ' : '') + "counts as magical";
+								}
 							}
-						},
-						"If I include 'Cold Imbued' in a melee weapon's name or description, it gets treated as a weapon that is imbued by my Myrmidon's Armament feature, adding my proficiency bonus as extra cold damage and counts as magical in the description. If I include 'Supercharged' and am at least level 10, the extra cold damage is doubled."
-					]
-				}
+						}
+					},
+					"If I include 'Imbued' in a melee weapon's name or description, it gets treated as a weapon that is imbued by my Myrmidon's Armament feature, adding my proficiency bonus as extra damage; at 7th level it adds counts as magical in the description. If I include 'Supercharged' and am at least level 10, the extra damage is doubled."
+				]
+			},
+			additional : levels.map(function (n) {
+				return (n < 3 ? "" : ((n < 18 ? 1 : 2) + " elemental attunement" + (n < 18 ? "" : "s")));
+			}),
+			extraname : "Myrmidon's Armament Attunement",
+			extrachoices : ["Acid", "Cold", "Fire", "Lightning", "Thunder"],
+			"acid" : {
+				name : "Myrmidon's Armament: Acid",
+				source : ["NB2:EM", 1],
+				description : desc([
+					"I am attuned to acid",
+					"Once per turn my weapon can deal extra acid damage equal to my prof. bonus",
+					"At 10th level I gain resistance to acid damage while wearing imbued armor"
+				]),
+				dmgres : ["Acid"],
+				prereqeval : elementPreReqFunction,
+				changeeval : elementChangeEvalFunction
+			},
+			"cold" : {
+				name : "Myrmidon's Armament: Cold",
+				source : ["NB2:EM", 1],
+				description : desc([
+					"I am attuned to cold",
+					"Once per turn my weapon can deal extra cold damage equal to my prof. bonus",
+					"At 10th level I gain resistance to cold damage while wearing imbued armor"
+				]),
+				dmgres : ["Cold"],
+				prereqeval : elementPreReqFunction,
+				changeeval : elementChangeEvalFunction
 			},
 			"fire" : {
 				name : "Myrmidon's Armament: Fire",
+				source : ["NB2:EM", 1],
 				description : desc([
-					"My melee weapon is fire imbued, it deals extra fire damage; counts as magical",
-					"The extra fire damage is equal to my proficiency bonus",
-					"When I finish a long rest, I can imbue a melee weapon I touch with an element",
-					"Use the \"Choose Feature\" button above to change the imbued type"
+					"I am attuned to fire",
+					"Once per turn my weapon can deal extra fire damage equal to my prof. bonus",
+					"At 10th level I gain resistance to fire damage while wearing my imbued armor"
 				]),
-				calcChanges : {
-					atkAdd : [
-						function (fields, v) {
-							if (v.isMeleeWeapon && !v.isNaturalWeapon && (/\b(fire imbued)\b/i).test(v.WeaponText)) {
-								var bonusDamage = ProficiencyBonusList[classes.totallevel - 1];
-								
-								if((/\b(supercharged?)\b/i).test(v.WeaponText) && classes.known.fighter.level >= 10)
-								{
-									bonusDamage *= 2;
-								}
-								
-								fields.Description += (fields.Description ? '; ' : '') + "+" + bonusDamage + " fire damage; counts as magical";
-							}
-						},
-						"If I include 'Fire Imbued' in a melee weapon's name or description, it gets treated as a weapon that is imbued by my Myrmidon's Armament feature, adding my proficiency bonus as extra fire damage and counts as magical in the description. If I include 'Supercharged' and am at least level 10, the extra fire damage is doubled."
-					]
-				}
+				dmgres : ["Fire"],
+				prereqeval : elementPreReqFunction,
+				changeeval : elementChangeEvalFunction
 			},
 			"lightning" : {
 				name : "Myrmidon's Armament: Lightning",
+				source : ["NB2:EM", 1],
 				description : desc([
-					"My melee weapon is lightning imbued, it deals extra lightning damage; counts as magical",
-					"The extra lightning damage is equal to my proficiency bonus",
-					"When I finish a long rest, I can imbue a melee weapon I touch with an element",
-					"Use the \"Choose Feature\" button above to change the imbued type"
+					"I am attuned to lightning",
+					"Once per turn my weapon can deal extra lightning damage equal to my prof. bonus",
+					"At 10th level I gain resistance to lightning damage while wearing my imbued armor"
 				]),
-				calcChanges : {
-					atkAdd : [
-						function (fields, v) {
-							if (v.isMeleeWeapon && !v.isNaturalWeapon && (/\b(lightning imbued)\b/i).test(v.WeaponText)) {
-								var bonusDamage = ProficiencyBonusList[classes.totallevel - 1];
-								
-								if((/\b(supercharged?)\b/i).test(v.WeaponText) && classes.known.fighter.level >= 10)
-								{
-									bonusDamage *= 2;
-								}
-								
-								fields.Description += (fields.Description ? '; ' : '') + "+" + bonusDamage + " lightning damage; counts as magical";
-							}
-						},
-						"If I include 'Lightning Imbued' in a melee weapon's name or description, it gets treated as a weapon that is imbued by my Myrmidon's Armament feature, adding my proficiency bonus as extra lightning damage and counts as magical in the description. If I include 'Supercharged' and am at least level 10, the extra lightning damage is doubled."
-					]
-				}
+				dmgres : ["Lightning"],
+				prereqeval : elementPreReqFunction,
+				changeeval : elementChangeEvalFunction
 			},
-			choiceDependencies : [{
-				feature : "subclassfeature7"
-			}, {
-				feature : "subclassfeature10"
-			}, {
-				feature : "subclassfeature15"
-			}, {
-				feature : "subclassfeature18"
-			}]
+			"thunder" : {
+				name : "Myrmidon's Armament: Thunder",
+				source : ["NB2:EM", 1],
+				description : desc([
+					"I am attuned to thunder",
+					"Once per turn my weapon can deal extra thunder damage equal to my prof. bonus",
+					"At 10th level I gain resistance to thunder damage while wearing my imbued armor"
+				]),
+				dmgres : ["Thunder"],
+				prereqeval : elementPreReqFunction,
+				changeeval : elementChangeEvalFunction
+			}
 		},
 		"subclassfeature7" : {
 			name : "Myrmidon's Bulwark",
 			source : ["NB2:EM", 1],
 			minlevel : 7,
 			description : desc([
-				"I can also imbue a suit of armor when I imbue a weapon during my ritual",
-				"While wearing it, I have resistance to the imbued damage type",
+				"I can also imbue a suit of armor when I imbue a weapon after my attunement",
+				"While wearing it, I have resistance to the imbued damage types",
+				"Additionally, my imbued weapon counts as magical",
 				"Use the \"Choose Feature\" button above to select the imbued type"
-			]),
-			choices : ["cold", "fire", "lightning"],
-			choicesNotInMenu : true,
-			"cold" : {
-				name : "Myrmidon's Bulwark: Cold",
-				description : desc([
-					"I can also imbue a suit of armor when I imbue a weapon during my ritual",
-					"While wearing the imbued armor, I am resistant to cold damage"
-				]),
-				dmgres : ["Cold"]
-			},
-			"fire" : {
-				name : "Myrmidon's Bulwark: Fire",
-				description : desc([
-					"I can also imbue a suit of armor when I imbue a weapon during my ritual",
-					"While wearing the imbued armor, I am resistant to fire damage"
-				]),
-				dmgres : ["Fire"]
-			},
-			"lightning" : {
-				name : "Myrmidon's Bulwark: Lightning",
-				description : desc([
-					"I can also imbue a suit of armor when I imbue a weapon during my ritual",
-					"While wearing the imbued armor, I am resistant to lightning damage"
-				]),
-				dmgres : ["Lightning"]
-			}
+			])
 		},
 		"subclassfeature10" : {
 			name : "Supercharge",
@@ -166,124 +168,48 @@ AddSubClass("fighter", "elemental myrmidon", {
 			minlevel : 10,
 			description : desc([
 				"As a bonus action, I can double the imbued extra damage dealt by my weapon",
-				"This effect lasts until the end of my turn",
-				"Use the \"Choose Feature\" button above to select the imbued type"
+				"When I deal this extra damage, I can add an effect based on type; See Notes page",
+				"This effect lasts until the end of my turn"
 			]),
-			choices : ["cold", "fire", "lightning"],
-			choicesNotInMenu : true,
-			"cold" : {
-				name : "Supercharge: Cold",
-				description : desc([
-					"As a bonus action, I can double the imbued extra cold damage dealt by my weapon",
-					"This effect lasts until the end of my turn"
-				])
-			},
-			"fire" : {
-				name : "Supercharge: Fire",
-				description : desc([
-					"As a bonus action, I can double the imbued extra fire damage dealt by my weapon",
-					"This effect lasts until the end of my turn"
-				])
-			},
-			"lightning" : {
-				name : "Supercharge: Lightning",
-				description : desc([
-					"As a bonus action, I can double the imbued extra lightning damage dealt by my weapon",
-					"This effect lasts until the end of my turn"
-				])
-			},
 			action : ["bonus action", ""],
 			recovery : "long rest",
 			usages : "Con mod per ",
 			usagescalc : "event.value = Math.max(1, What('Con Mod'));",
+			toNotesPage : [{
+				name : "Supercharge Effects Table",
+				source : ["NB2:EM", 1],
+				popupName : "Supercharge Effects Table",
+				note : [
+					"Type\tEffect",
+					"acid\tTarget's AC is reduced by 2 until the end of its next turn",
+					"cold\tTarget's speed is reduced by 10 feet until the start of your next turn",
+					"fire\tTarget takes 1d8 fire damage at the start of its next turn",
+					"lightning\tTarget can't take reactions until the start of its next turn",
+					"thunder\tTarget is pushed 10 feet away from you"
+				]
+			}]
 		},
 		"subclassfeature15" : {
-			name : "Elemental Fluidity",
+			name : "Elemental Flux",
 			source : ["NB2:EM", 1],
 			minlevel : 15,
 			usages : 1,
 			recovery : "long rest",
 			description : desc([
-				"When I take cold/fire/lightning damage I can become immune to it as a reaction",
-				"To use this reaction, the type taken must a different type than my imbued type",
-				"As part of the reaction, I can change the imbued type of my weapon and armor",
-				"If I do, the imbued type becomes the damage type I became immune to",
-				"Use the \"Choose Feature\" button above to select the imbued type"
+				"When I take damage, I can use a reaction to change an attuned type",
+				"If I do, I choose another element I am not attuned to",
+				"The imbued type of my weapon and armor also changes to the new type",
+				"The new resistance gained from changing applies to the triggering damage"
 			]),
-			choices : ["cold", "fire", "lightning"],
-			choicesNotInMenu : true,
-			"cold" : {
-				name : "Elemental Fluidity",
-				description : desc([
-					"When I take fire or lightning, damage I can become immune to it as a reaction",
-					"As part of the reaction, I can change the imbued type of my weapon and armor",
-					"If I do, the imbued type becomes the damage type I became immune to",
-					"Use the \"Choose Feature\" button above to change the imbued type"
-				]),
-				action : ["reaction", "(fire/lightning damage)"]
-			},
-			"fire" : {
-				name : "Elemental Fluidity",
-				description : desc([
-					"When I take cold or lightning, damage I can become immune to it as a reaction",
-					"As part of the reaction, I can change the imbued type of my weapon and armor",
-					"If I do, the imbued type becomes the damage type I became immune to",
-					"Use the \"Choose Feature\" button above to change the imbued type"
-				]),
-				action : ["reaction", "(cold/lightning damage)"]
-			},
-			"lightning" : {
-				name : "Elemental Fluidity",
-				description : desc([
-					"When I take cold or fire damage, I can become immune to it as a reaction",
-					"As part of the reaction, I can change the imbued type of my weapon and armor",
-					"If I do, the imbued type becomes the damage type I became immune to",
-					"Use the \"Choose Feature\" button above to change the imbued type"
-				]),
-				action : ["reaction", "(cold/fire damage)"]
-			}
+			action : ["reaction", " (when damaged)"]
 		},
 		"subclassfeature18" : {
 			name : "Myrmidon's Bastion",
 			source : ["NB2:EM", 1],
 			minlevel : 18,
 			description : desc([
-				"While wearing my imbued armor, I gain immunity to that damage type",
-				"I also become resistant to nonmagical bludgeoning, piercing, and slashing damage",
-				"Use the \"Choose Feature\" button above to select the imbued type"
-			]),
-			choices : ["cold", "fire", "lightning"],
-			choicesNotInMenu : true,
-			"cold" : {
-				name : "Myrmidon's Bastion: Cold",
-				description : desc([
-					"While wearing my cold imbued armor, I gain the following benefits:",
-					" \u2022 Immunity to cold damage",
-					" \u2022 Resistance to nonmagical bludgeoning, piercing, and slashing damage"
-				]),
-				savetxt : { immune : ["cold"] },
-				dmgres : [["Bludgeoning", "Bludg. (nonmagical)"], ["Piercing", "Pierc. (nonmagical)"], ["Slashing", "Slash. (nonmagical)"]]
-			},
-			"fire" : {
-				name : "Myrmidon's Bastion: Fire",
-				description : desc([
-					"While wearing my fire imbued armor, I gain the following benefits:",
-					" \u2022 Immunity to fire damage",
-					" \u2022 Resistance to nonmagical bludgeoning, piercing, and slashing damage"
-				]),
-				savetxt : { immune : ["fire"] },
-				dmgres : [["Bludgeoning", "Bludg. (nonmagical)"], ["Piercing", "Pierc. (nonmagical)"], ["Slashing", "Slash. (nonmagical)"]]
-			},
-			"lightning" : {
-				name : "Myrmidon's Bastion: Lightning",
-				description : desc([
-					"While wearing my lightning imbued armor, I gain the following benefits:",
-					" \u2022 Immunity to lightning damage",
-					" \u2022 Resistance to nonmagical bludgeoning, piercing, and slashing damage"
-				]),
-				savetxt : { immune : ["lightning"] },
-				dmgres : [["Bludgeoning", "Bludg. (nonmagical)"], ["Piercing", "Pierc. (nonmagical)"], ["Slashing", "Slash. (nonmagical)"]]
-			}
+				"I can attune to two different elements instead of one"
+			])
 		}
 	}
 });
